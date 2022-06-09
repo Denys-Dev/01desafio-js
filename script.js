200d88aa9098924e21d9569c0103f4b1219d9547
Denys-Dev <mrdenys18@gmail.com>
2022-06-09 20:32:28 -0300

    primeiro desafio de js

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Denilson";
+let numero = 24;
+let anoAtual = 2022;
+let quartaFeira = "false"
+
+
+console.log(nome);
+console.log(numero);
+console.log( anoAtual - numero);
+console.log(typeof quartaFeira);
+
+console.log( "Estou mexendo no console.log");
